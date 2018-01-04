3c2e63ad2da90e5ddb1103e17db8f28cb308d407
toutyio <lifuzhen@touty.io>
2018-01-04 16:24:33 +0800

    connection mysql

--- src/node/init.js
+++ src/node/init.js
@@ -1,23 +1,40 @@
 /**
  * Created by fuzhen on 2017/12/28.
  */
-let mysql = require('mysql');
+let express = require("express");  //映入express  没有的下载  可以在网上找百度
+let mysql = require('mysql');    //引入mysql
 
-let connection=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'123456',
-    port:'3306',
-    database:'mydb'
+let app=express();
+
+app.use(express.static(__dirname + "/public"));  //公共  目录  定义
+
+//配置数据库信息
+let connection = mysql.createConnection({
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : '123456',
+    database : 'mydb'
 });
 
-connection.connect(function (err) {
-    if(err){
-        console.log("mysql"+err);
-    }
-});
-// let selSql = 'SELECT * FROM users';
-// connection.query(selSql,(err,result)=>{
-//     console.log(result);
-// })
+//连接
+connection.connect();
 
+//路径get请求
+app.get("/",function(req,res){
+    //跨域
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    res.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
+
+    //查询
+    connection.query('SELECT * FROM users', function (err,data) {
+        if(err){
+            console.log(err);
+        }else{
+            // console.log(JSON.stringify(data));  //打印数据
+            res.end(JSON.stringify(data));   //转化成字符串
+        }
+    });
+});
+
+app.listen(4848);
