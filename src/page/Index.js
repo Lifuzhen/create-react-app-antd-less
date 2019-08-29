83367af21e1e7c018fdcc7220bc514353252a424
李福祯 <1342786307@qq.com>
2019-08-29 13:11:02 +0800

    css中filter属性的应用

--- src/page/Index.js
+++ src/page/Index.js
@@ -7,16 +7,18 @@ import {Link} from "react-router-dom";
 import G2 from "g2";
 import $ from "jquery";
 import Service from "../data/Service";
+import logo from "../assets/logo.png";
+import filter from "../assets/filter.png";
 import ScrollReveal from "scrollreveal";
 
-class Index extends React.Component{
+class Index extends React.Component {
 
     componentDidMount() {
         var data = [
-            {name: 'Microsoft Internet Explorer', value: 56.33 },
+            {name: 'Microsoft Internet Explorer', value: 56.33},
             {name: 'Chrome', value: 24.03},
             {name: 'Firefox', value: 10.38},
-            {name: 'Safari',  value: 4.77},
+            {name: 'Safari', value: 4.77},
             {name: 'Opera', value: 0.91},
             {name: 'Proprietary or Undetectable', value: 0.2}
         ];
@@ -34,8 +36,8 @@ class Index extends React.Component{
         chart.legend('name', {
             position: 'bottom',
             itemWrap: true,
-            formatter: function(val) {
-                for(var i = 0, len = data.length; i < len; i++) {
+            formatter: function (val) {
+                for (var i = 0, len = data.length; i < len; i++) {
                     var obj = data[i];
                     if (obj.name === val) {
                         return val + ': ' + obj.value + '%';
@@ -52,7 +54,7 @@ class Index extends React.Component{
         chart.intervalStack()
             .position(Stat.summary.percent('value'))
             .color('name')
-            .label('name*..percent',function(name, percent){
+            .label('name*..percent', function (name, percent) {
                 percent = (percent * 100).toFixed(2) + '%';
                 return name + ' ' + percent;
             });
@@ -63,34 +65,39 @@ class Index extends React.Component{
         geom.setSelected(items[1]); // 设置选中
 
 
-
-
-        ScrollReveal().reveal('.left',{
+        ScrollReveal().reveal('.left', {
             duration: 1000,
             distance: '40px',
             easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
             origin: 'left',
             interval: 150
         });
-        ScrollReveal().reveal('.right',{
+        ScrollReveal().reveal('.right', {
             duration: 1000,
             distance: '40px',
             easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
             origin: 'right',
             interval: 150
         });
-        ScrollReveal().reveal('.bottom',{
-            duration: 1000,
-            distance: '40px',
+        ScrollReveal().reveal('.bottom', {
+            duration: 1500,
+            distance: '80px',
             easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
-            interval: 100,
             origin: 'bottom',
-            scale: 0.9,
-            viewFactor: 0.5
-        })
+            interval: 200
+        });
+        // ScrollReveal().reveal('.bottom',{
+        //     duration: 1000,
+        //     distance: '40px',
+        //     easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
+        //     interval: 100,
+        //     origin: 'bottom',
+        //     scale: 0.9,
+        //     viewFactor: 0.5
+        // })
     }
 
-    render(){
+    render() {
         return <div>
             <div className="left">
                 测试scrollReveal——左
@@ -106,15 +113,75 @@ class Index extends React.Component{
             <div id="c1"/>
             <Link to="/formMap">ant Design中form动态渲染的增删测试</Link>
 
-
             <br/>
             <br/>
             <br/>
 
             <div className="bottom">
-                <img src="../assets/logo.png"/>
+                <img src={logo}/>
+            </div>
+
+            <div className="left">
+                <img src={logo}/>
+            </div>
+
+            <div className="right">
+                <img src={logo}/>
+            </div>
+
+            <div className="test-css-filter">
+                <div className="none">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+                <div className="blur">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+                <div className="brightness">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+                <div className="contrast">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+                <div className="drop-shadow">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+                <div className="grayscale">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+                <div className="hue-rotate">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+                <div className="invert">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+                <div className="opacity">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+                <div className="saturate">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+                <div className="sepia">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+                <div className="url">
+                    <img src={filter}/>
+                    <img src={filter}/>
+                </div>
+
             </div>
         </div>
     }
 }
+
 export default Index;
